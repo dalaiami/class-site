de013c118e907eb61f41c66301391a1256c656c7
dalaiami <59937318+dalaiami@users.noreply.github.com>
2020-03-25 00:02:15 -0400

    .

--- exercises/javascript-sketch/js/functions.js
+++ exercises/javascript-sketch/js/functions.js
@@ -3,10 +3,23 @@ function toggle() {
     element.classList.toggle("open");
     }
 
-    $('.marquee').marquee({
-    	duration: 8000,
-    	gap: 50,
-    	delayBeforeStart: 0,
-    	direction: 'right',
-    	duplicated: false
-    });
+    // $('.marquee').marquee({
+    // 	duration: 8000,
+    // 	gap: 50,
+    // 	delayBeforeStart: 0,
+    // 	direction: 'right',
+    // 	duplicated: false
+    // });
+
+    function setup() {
+      createCanvas(1440, 790);
+      background(255, 255, 255);
+    }
+
+    function draw() {
+      stroke(255, 255, 0);
+      strokeWeight(60);
+      if (mouseIsPressed === true) {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+      }
+    }
