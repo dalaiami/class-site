f5840633dbf1e96469bd20311b821c5e60403b69
dalaiami <59937318+dalaiami@users.noreply.github.com>
2020-03-24 20:13:05 -0400

    js

---
+++ exercises/javascript-exercise/js/functions.js
@@ -0,0 +1,12 @@
+function toggle() {
+    var element = document.getElementById("box");
+    element.classList.toggle("open");
+    }
+
+    $('.marquee').marquee({
+    	duration: 8000,
+    	gap: 50,
+    	delayBeforeStart: 0,
+    	direction: 'right',
+    	duplicated: false
+    });
